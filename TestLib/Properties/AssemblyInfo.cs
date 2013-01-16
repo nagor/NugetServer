66550f28b5319bfe05b52e4e93047d93c42c326b
roman.nagorkin <roman.nagorkin@sibers.com>
2013-01-16 23:14:30 +0700

    -ignorings, assembly info, nuspec

--- TestLib/Properties/AssemblyInfo.cs
+++ TestLib/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestLib")]
 [assembly: AssemblyDescription("Test lib. Contains TestLibSettings class, Settings controller and view")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Our company")]
+[assembly: AssemblyCompany("Nagor")]
 [assembly: AssemblyProduct("TestLib")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
